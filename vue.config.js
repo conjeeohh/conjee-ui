319c42f9c984dceabb713d5e68c534aeb7e50a34
粥太浓了 <zhoubao57@gmail.com>
2021-09-15 11:49:51 +0800

    -

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,7 @@ module.exports = {
     },
     // 扩展 webpack 配置，使 packages 加入编译
     chainWebpack: config => {
+        config.entry('app').clear().add('./examples/main.js')
         // @ 默认指向 src 目录，这里要改成 examples
         // 另外也可以新增一个 ~ 指向 packages
         config.resolve.alias
